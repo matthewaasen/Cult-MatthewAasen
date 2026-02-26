58b18ddba2d89d22f794b8555b376833ee655e71
matthewaasen <matthewaasen05@gmail.com>
2026-02-26 14:39:21 -0600

    added HP sliders

--- Assets/Scripts/PlayerHPController.cs
+++ Assets/Scripts/PlayerHPController.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPSliderController : MonoBehaviour
 {
+    private Slider slider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider = gameObject.GetComponent<Slider>();
+        slider.value = 45;
     }
 
     // Update is called once per frame
